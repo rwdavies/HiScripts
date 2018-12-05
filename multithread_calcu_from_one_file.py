9bac3fd5639a99d7812a007f9280f7d3511bd854
Zilong-Li <zimusen94@gmail.com>
2018-12-05 17:22:10 +0800

    initial add two py file

---
+++ multithread_calcu_from_one_file.py
@@ -0,0 +1,71 @@
+# coding:utf-8
+import os
+import sys
+import gzip
+import re
+import multiprocessing as mp
+from calcu_hwe_info_eaf import *
+
+
+def calculate_info(lines):
+    out = []
+    for line in lines:
+        if re.match('^#', line):
+            continue
+        tmp = line.rstrip().split('\t')
+        gps = array([map(lambda gi:float(gi), g.split(':')[1].split(','))
+                     for g in tmp[9:]])
+
+        out.append("\t".join(tmp[0:5]) + "\t" + get_hwe_info_eaf(gps) + "\n")
+
+    return out
+
+
+def process_wrapper(filename, chunkStart, chunkSize):
+
+    # print 'subprocess pid is %s ' % os.getpid()
+    with open(filename) as f:
+        f.seek(chunkStart)
+        lines = f.read(chunkSize).splitlines()
+        out = calculate_info(lines)
+    return out
+
+
+def chunkify(filename, size=1024 * 1024):
+    fileEnd = os.path.getsize(filename)
+
+    with open(filename, 'r') as f:
+
+        chunkEnd = f.tell()
+        while True:
+            chunkStart = chunkEnd
+            f.seek(size, 1)
+            f.readline()
+            chunkEnd = f.tell()
+            yield chunkStart, chunkEnd - chunkStart
+            if chunkEnd > fileEnd:
+                break
+
+
+def main():
+    vcf = sys.argv[1]
+    cores = 4
+    pool = mp.Pool(cores)
+    jobs = []
+    size = 1024 * 1024 * 10  # process 40 lines per job
+
+    for chunkStart, chunkSize in chunkify(vcf, size):
+        jobs.append(pool.apply_async(
+            process_wrapper, (vcf, chunkStart, chunkSize)))
+
+    output = sys.argv[2]
+    o = open(output, 'w')
+    for job in jobs:
+        o.writelines(job.get())
+
+    pool.close()
+    o.close()
+
+
+if __name__ == '__main__':
+    main()
